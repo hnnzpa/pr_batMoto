9db34f20e9b998915e2e2f57cc4f490e220b64cd
math1913 <mateohuertas21@gmail.com>
2025-11-05 09:50:09 +0100

    frena cuando la velocidad es 0

--- Main.py
+++ Main.py
@@ -4,9 +4,9 @@ import serial
 import ArduinoReader
 
 # --- CONFIGURACIÓN ---
-VIDEO_PATH = "videosideconmoto.mp4"
+VIDEO_PATH = "carreteraLoop.mp4"
 PORT = "COM5"
-MIN_SPEED = 1.0
+MIN_SPEED = 0.0
 MAX_SPEED = 15.0
 
 def map_range(x, in_min, in_max, out_min, out_max):
@@ -15,45 +15,60 @@ def map_range(x, in_min, in_max, out_min, out_max):
 
 def main():
     instance = vlc.Instance()
-    player = instance.media_player_new()
+    # player = instance.media_player_new()
+    # media = instance.media_new(VIDEO_PATH)
+    # player.set_media(media)
+    media_list = instance.media_list_new()
     media = instance.media_new(VIDEO_PATH)
-    player.set_media(media)
-    player.play()
+    media_list.add_media(media)
+    
+    list_player = instance.media_list_player_new()
+    media_player = instance.media_player_new()
+
+    list_player.set_media_list(media_list)
+    list_player.set_media_player(media_player)
+
+    list_player.set_playback_mode(vlc.PlaybackMode.loop)
+    list_player.play()
     
     # Espera a que comience la reproducción
     time.sleep(2)
-    while player.get_state() != vlc.State.Playing:
+    while list_player.get_state() != vlc.State.Playing:
         time.sleep(0.1)
 
     # arduino = ArduinoReader(PORT)
-    ultima_velocidad = None
+    valor = ArduinoReader.leer_arduino()
+    ultima_velocidad = map_range(valor, 1, 100, MIN_SPEED, MAX_SPEED)
 
     print("🎥 Control de velocidad con potenciómetro (Ctrl+C para salir)")
-
+    
     try:
         while True:
             valor = ArduinoReader.leer_arduino()
             if valor is not None:
-                velocidad = map_range(valor, 0, 100, MIN_SPEED, MAX_SPEED)
-                if ultima_velocidad is None or abs(velocidad - ultima_velocidad) > 0.05:
-                    player.set_rate(velocidad)
-                    ultima_velocidad = velocidad
+                velocidad = map_range(valor, 1, 100, MIN_SPEED, MAX_SPEED)
+                if (abs(velocidad - ultima_velocidad) > 0.05): #Solo cambia la velocidad del video
+                    media_player.set_rate(velocidad)
                     print(f"🔧 Velocidad actual: {velocidad:.2f}x")
-            time.sleep(0.1)
-            
-            state = player.get_state()
-            if state == vlc.State.Ended:
-                # player.stop()
-                player.set_media(media)
-                player.play()
+            # time.sleep(0.1)
+
+            if ((velocidad <= 0) and ( velocidad != ultima_velocidad)): # Si la velocidad es 0 y venia de estar en marcha se pausa
+                media_player.pause()
+            else:
+                if ( velocidad != ultima_velocidad): #Si cambio pero la vel no es 0 lo pongo en marcha
+                    media_player.play()
+            ultima_velocidad = velocidad
+            # state = player.get_state()
+            # if state == vlc.State.Ended:
+            #     player.set_media(media)
+            #     player.play()
 
     except KeyboardInterrupt:
         print("\n🛑 Reproducción detenida por el usuario.")
     except serial.SerialException:
         print("\n⚠️ Error de conexión con Arduino.")
     finally:
-        # arduino.cerrar()
-        player.stop()
+        list_player.stop()
 
 if __name__ == "__main__":
     main()
